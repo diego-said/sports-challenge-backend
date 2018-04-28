d482173eddd0dfe0d2b562b5aa0a8bcb8ce76065
Diego Said <diego@doublelogic.com.br>
2018-04-28 10:57:53 -0300

    initial version of fans service

---
+++ fans-service/src/main/java/br/com/doublelogic/sportschallengebackend/fansservice/FansServiceApplication.java
@@ -0,0 +1,12 @@
+package br.com.doublelogic.sportschallengebackend.fansservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FansServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FansServiceApplication.class, args);
+	}
+}
